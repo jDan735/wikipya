e67482dcd93cf32cd690aa01f777800f95c71a4f
Daniel Zakharov <daniel734@bk.ru>
2021-11-27 22:17:42 +0300

    remove configurations from get_all

--- wikipya/clients/mediawiki.py
+++ wikipya/clients/mediawiki.py
@@ -96,18 +96,17 @@ class MediaWiki(BaseClient):
         except AttributeError:
             raise NotFound(f"Not found page with this id: {id}")
 
-    async def get_all(self, query, lurk=False,
-                      blocklist=(), prefix="w", img_blocklist=(), **kwargs):
+    async def get_all(self, query, lurk=False, prefix="w"):
         search = await self.search(query)
         opensearch = await self.opensearch(
             query if lurk else search[0].title
         )
 
         result = opensearch.results[0]
-        page = await self.page(result.title, blocklist=blocklist)
+        page = await self.page(result.title)
 
         try:
-            image = await self.image(page.title, prefix=prefix, img_blocklist=img_blocklist)
+            image = await self.image(page.title, prefix=prefix)
             image = WRW_FLAG if image.source == WGR_FLAG else image.source
 
         except Exception:

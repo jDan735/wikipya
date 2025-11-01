32c846119ec0ecad85e23b0a51c59e2d8c063231
Daniel Zakharov <gzdan734@gmail.com>
2025-11-01 19:44:20 +0300

    add user-agent

--- wikipya/clients.py
+++ wikipya/clients.py
@@ -8,6 +8,12 @@ from .constants import TAG_BLOCKLIST, DEFAULT_PARAMS
 from .exceptions import ParseError
 
 
+HEADERS: dict[str, str] = {
+    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+    "Accept-Encoding": "gzip",
+}
+
+
 class BaseClient(BaseModel):
     url: MediawikiUrl
     timeout: Optional[int] = 5
@@ -15,31 +21,34 @@ class BaseClient(BaseModel):
     tag_blocklist: list[str] = Field(repr=False, default=TAG_BLOCKLIST)
     default_params: dict[str, str | int] = Field(repr=False, default=DEFAULT_PARAMS)
 
+    client: Any | None = None
+
+    def model_post_init(self, __context: Any) -> None:
+        self.client = httpx.AsyncClient(timeout=self.timeout, headers=HEADERS)
+
     async def get(
         self, url: Optional[str] = None, **params: Any
     ) -> tuple[httpx.Response, Any]:
-        async with httpx.AsyncClient(timeout=self.timeout) as client:
-            res: httpx.Response = await client.get(
-                str(url or self.url),
-                params={**self.default_params, **params},
-                follow_redirects=True,
-            )
+        res: httpx.Response = await self.client.get(
+            str(url or self.url),
+            params={**self.default_params, **params},
+            follow_redirects=True,
+        )
 
         json: Any = res.json()
 
         try:
             if error := json.get("error"):
-                raise ParseError(f'{error["code"]}: {error["info"]}')
+                raise ParseError(f"{error['code']}: {error['info']}")
         except:  # noqa: E722
             pass
 
         return res, json
 
     async def get_html(self, url: Optional[str] = None, **params: Any):
-        async with httpx.AsyncClient(timeout=self.timeout) as client:
-            return await client.get(
-                str(url or self.url), params=params, follow_redirects=True
-            )
+        return await self.client.get(
+            str(url or self.url), params=params, follow_redirects=True
+        )
 
 
 class MediaWiki(BaseClient):
